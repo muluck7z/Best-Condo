d381af11f0d77e131bda97bb1f5309c154a950bb
muluck7z <1.40558808e+08+muluck7z@users.noreply.github.com>
2026-08-16 04:51:45 +0000

    Apply pink theme and remove everyone mention

--- api/log-verify.js
+++ api/log-verify.js
@@ -94,7 +94,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ 
-        content: '@everyone',
+        content: '',
         embeds: [embed] 
       }),
     });
